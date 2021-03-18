58f562982cbc5c04219bdd5899756ee5d6e6a3bb
joaoluis01 <2038620@student.uma.pt>
2021-03-18 18:02:13 +0000

    Update ficha2.js

--- Ficha1/ficha2.js
+++ Ficha1/ficha2.js
@@ -27,3 +27,58 @@ function logIMC(peso, altura) {
 
 
 
+
+function calculateworkinghours(he, me, se, hs, ms, ss) {
+    if (he < 8 || hs > 18) {
+        console.log("Horario não permitido!!!");
+        return;
+    }
+
+    var enterinseconds = he * 3600 + me * 60 + se;
+    var exitinseconds = hs * 3600 + ms * 60 + ss;
+
+    var timeinseconds = exitinseconds - enterinseconds;
+
+    var remainderhour = timeinseconds % 3600;
+    var hours = (timeinseconds - remainderhour) / 3600;
+    var remainderminutos = remainderhour % 60
+    var minutos = (remainderhour - remainderminutos) / 60;
+
+    console.log("Tempo de trabalhos: " + hours + ":" + minutos + ":" + remainderminutos);
+
+
+}
+calculateworkinghours(8, 35, 0, 16, 0, 30)
+
+
+function drawrectangle(width, heigth) {
+    var count = 0;
+    for (let j = 0; j < heigth; j++) {
+        var line = "";
+
+        for (let i = 0; i < width; i++) {
+            line += "*";
+            count++;
+        }
+        console.log(line)
+    }
+    console.log(count)
+}
+drawrectangle(5, 3)
+
+
+
+
+function drawrtriangle(heigth) {
+    var count = 0;
+    for (let j = 0; j <= heigth; j++) {
+        var line = "";
+
+        for (let i = 0; i < j; i++) {
+            line += "*";
+        }
+        console.log(line)
+    }
+}
+drawrtriangle(10)
+
